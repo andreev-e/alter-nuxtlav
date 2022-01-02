4461f1e804cb77d577fa5a1617a9e040f7fb06a8
Evgeniy Andreev <andreev-e@mail.ru>
2022-01-02 12:02:25 +0300

    ecosys

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,11 @@ module.exports = {
       {
         name: 'Altertravel',
         exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
+        instances: '1', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
         args: 'start',
         watch: true,
-        max_memory_restart: '400M'
+        // max_memory_restart: '400M'
       }
     ]
   }
