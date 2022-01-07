022d64c4301274f53c0c77fa5321d477d80058e9
Evgeniy Andreev <andreev-e@mail.ru>
2022-01-07 14:40:21 +0300

    noport

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
     apps: [
       {
         name: 'Altertravel',
-        port: 3000,
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
